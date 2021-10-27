76d718463cb75994d41608f2de1b692264e66378
SNValadez <snvaladez13@gmail.com>
2021-10-27 14:27:29 -0700

    app.get party id

--- server.js
+++ server.js
@@ -74,6 +74,21 @@ app.get('/api/parties', (req, res) => {
     });
   });
 
+  app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
