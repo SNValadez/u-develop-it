fe4fbef3bc99bef0be7d0edafbf1d44b777ea0a3
SNValadez <snvaladez13@gmail.com>
2021-10-26 22:54:41 -0700

    ports and routes

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
