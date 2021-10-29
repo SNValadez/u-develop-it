aadba0b424a82d5d96499daea9bff2bf293ff943
SNValadez <snvaladez13@gmail.com>
2021-10-28 17:56:39 -0700

    db request server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
-const inputCheck = require('./utils/inputCheck');
+
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const db = require("./db/connection");
 const apiRoutes = require("./routes/apiRoutes");
+const inputCheck = require('./utils/inputCheck');
 
 app.use("/api", apiRoutes);
 
@@ -17,55 +18,7 @@ app.use(express.json());
 
 
 
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
 
-  app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-  });
-
-  app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        // checks if anything was deleted
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Party not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
-      }
-    });
-  });
 
   
 // Create a candidate
@@ -87,7 +40,10 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
-
+db.connect(err => {
+    if (err) throw err;
+    console.log("Database connected.");
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
